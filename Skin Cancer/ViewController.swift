75066d3371ec13ec4b27f149bae549ca507ff86c
Sergey Blintsov <sblin180@gmail.com>
2018-09-06 18:01:27 +0300

    Initial Commit

---
+++ Skin Cancer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skin Cancer
+//
+//  Created by Blintsov Sergey on 06/09/2018.
+//  Copyright © 2018 Blintsov Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
