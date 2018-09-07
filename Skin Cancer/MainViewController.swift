97cd8bc31b8cd0dc4e27bb53272b42dd73189b9b
Sergey Blintsov <sblin180@gmail.com>
2018-09-07 20:54:30 +0300

    Disclaimer released

---
+++ Skin Cancer/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  Skin Cancer
+//
+//  Created by Blintsov Sergey on 07/09/2018.
+//  Copyright © 2018 Blintsov Sergey. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
