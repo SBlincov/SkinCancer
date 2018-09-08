5c26900688a72783f03471649796f3f535b7f8cb
Sergey Blintsov <sblin180@gmail.com>
2018-09-08 17:31:05 +0300

    Take a picture

--- Skin Cancer/MainViewController.swift
+++ Skin Cancer/MainViewController.swift
@@ -7,29 +7,114 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var scene: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func camButtonTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController() // 1
+        imagePicker.delegate = self // 2
+        imagePicker.sourceType = UIImagePickerControllerSourceType.camera // 3
+        
+        // для выбора только фотокамеры, не для записи видео
+        imagePicker.showsCameraControls = true // 4
+        
+        self.present(imagePicker, animated: true, completion: nil) // 5
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: false)
-        // Do any additional setup after loading the view.
+        
+        super.viewDidLoad()
+        
+        guard let image = UIImage(named: "train_night") else {
+            fatalError("no starting image")
+        }
+        
+        scene.image = image
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectScene(image: ciImage)
     }
+        // Do any additional setup after loading the view.
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension MainViewController {
+    @IBAction func pickImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .savedPhotosAlbum
+        present(pickerController, animated: true)
+    }
+}
+extension MainViewController: UINavigationControllerDelegate {
+    
+}
+
+extension MainViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true)
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("couldn't load image from Photos")
+        }
+        
+        scene.image = image
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectScene(image: ciImage)
+        
+    }
+}
+
+extension MainViewController {
+    func detectScene(image: CIImage) {
+        answerLabel.text = "detecting scene..."
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: melanoma().model) else {
+            fatalError("can't load Places ML model")
+        }
+        
+        // Create a Vision request with completion handler
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.answerLabel.text = "\(Int(topResult.confidence * 100))% it's a \(topResult.identifier)"
+            }
+        }
+        
+        // Run the Core ML GoogLeNetPlaces classifier on global dispatch queue
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
+        
+    }
+}
+
